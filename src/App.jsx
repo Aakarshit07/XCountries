16838ad96ad2b6368b99991fb9cfc27f1af0bd07
Aakarshit07 <aakarshit070@gmail.com>
2024-03-26 17:58:18 +0530

    Initial Solution of Xcountries

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react'
+
+function App() {
+  const [countries, setCountries] = useState([]);
+  
+  useEffect(() => {
+    if(countries){
+      fetchCountries();
+    }
+  });
+
+  const fetchCountries = async () => {
+    const data = await fetch("https://restcountries.com/v3.1/all");
+    const jsonData = await data.json();
+    
+    setCountries(jsonData);
+  }
+
+  return (
+    <>
+      <div className='country-container'>
+        {countries.map((country) => (
+          <div className='country-card' key={country.cca3}>
+            <img className='country-img' src={country.flags.png} alt={country.name.common} />
+            <h2>{country.name.common}</h2>
+          </div>
+        ))}
+      </div>
+    </>
+  )
+}
+
+export default App;
